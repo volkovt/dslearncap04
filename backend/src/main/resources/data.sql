b73e52de9f077224b3b3a93f4ddf2376ff4caf0e
Diego Melo <diego_melo_1991@hotmail.com>
2022-01-04 11:22:57 -0300

    Course

--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -12,3 +12,5 @@ INSERT INTO tb_user_role (user_id, role_id) VALUES (2, 2);
 INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 1);
 INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 2);
 INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 3);
+
+INSERT INTO tb_course (name, img_uri, img_gray_uri) VALUES ('Bootcamp HTML', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTENFZJyWBQp0CiCF_qsc2ikVDT4MdjSsXb9A&usqp=CAU', 'https://live.staticflickr.com/3776/13477561034_a447b51b47_c.jpg');
